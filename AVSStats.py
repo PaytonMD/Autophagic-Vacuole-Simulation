6b6e04ae455044ae4d5a2ed439a5cccae14eb97c
PaytonMD <payton.station@gmail.com>
2021-02-03 19:48:26 -0500

    Forgot to actually add the AVSStats file on the last commit/push. This script is a non-functional shell at the moment, but will soon be updated to have statistical functionality of SliceStats body area data.

---
+++ AVSStats.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+############################################################################################################
+#   Eastern Michigan University
+#   Backues Lab  
+#   Author: Payton Dunning
+#   Last Date Modified: February 3rd, 2021
+#
+#   DESCRIPTION
+############################################################################################################
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+############################################################################################################
+
+def main():
+    print("Please select a statistical script to run from the following 3 options: ")
+    print("[1]: Find average and mean body size")
+    print("[2]: Q-Q (quantile-quantile) plot")
+    print("[3]: Perform KS (Kolmogorov-Smirnov test)")
+    print("[0]: Exit Script")
+
+main()
