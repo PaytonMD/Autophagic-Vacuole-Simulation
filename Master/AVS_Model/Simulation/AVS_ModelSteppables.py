5fc32fb125b83a160d3508bd8e021f07fb27897c
PaytonMD <payton.station@gmail.com>
2021-04-01 03:09:59 -0400

    Added CC3D import statement needed to use 'CompuCellSetup'.

--- Master/AVS_Model/Simulation/AVS_ModelSteppables.py
+++ Master/AVS_Model/Simulation/AVS_ModelSteppables.py
@@ -1,11 +1,11 @@
-
 from cc3d.core.PySteppables import *
+from cc3d import CompuCellSetup
 
 class AVS_ModelSteppable(SteppableBasePy):
 
     def __init__(self,frequency=1):
 
-        SteppableBasePy.__init__(self,frequency)
+        Steppab-leBasePy.__init__(self,frequency)
 
     def start(self):
         """
